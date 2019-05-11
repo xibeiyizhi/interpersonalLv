d3e6ee34d5eda387d21a8f31d3b2d2298a6a777a
zheng jia <zhengjia@zhengdeMacBook-Air.local>
2019-05-11 22:52:01 +0800

    fxi

--- src/main/java/club/own/site/controller/IndexController.java
+++ src/main/java/club/own/site/controller/IndexController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
@@ -12,11 +13,16 @@ public class IndexController extends BaseController {
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView index() throws Exception {
-        if(log.isInfoEnabled()){
-            log.info("index page...");
-        }
         ModelAndView mav = new ModelAndView();
         mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public ModelAndView detail(@RequestParam(name = "id") String name) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name", name);
+        mav.setViewName("detail");
         return mav;
     }
 }
