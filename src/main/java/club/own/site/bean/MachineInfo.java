7e343e689d7711e8403ea818c7f8cf97cb8d0ed3
贾正 <jiazheng@jiazhengdeMacBook-Pro.local>
2019-05-19 19:16:15 +0800

    fix

--- src/main/java/club/own/site/bean/MachineInfo.java
+++ src/main/java/club/own/site/bean/MachineInfo.java
@@ -5,11 +5,42 @@ import lombok.Data;
 
 @Data
 public class MachineInfo {
-    private int ratioCPU;
     private double totalMemory;
     private double useMemory;
     private double freeMemory;
-    private String ratioMemory;
+    private double ratioMemory;
+
+    public double getTotalMemory() {
+        return totalMemory;
+    }
+
+    public void setTotalMemory(double totalMemory) {
+        this.totalMemory = totalMemory;
+    }
+
+    public double getUseMemory() {
+        return useMemory;
+    }
+
+    public void setUseMemory(double useMemory) {
+        this.useMemory = useMemory;
+    }
+
+    public double getFreeMemory() {
+        return freeMemory;
+    }
+
+    public void setFreeMemory(double freeMemory) {
+        this.freeMemory = freeMemory;
+    }
+
+    public double getRatioMemory() {
+        return ratioMemory;
+    }
+
+    public void setRatioMemory(double ratioMemory) {
+        this.ratioMemory = ratioMemory;
+    }
 
     public String toString(){
         return JSON.toJSONString(this);
