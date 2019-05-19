3753e6195d6cd1049a4cfa971c305f4c49371c04
贾正 <jiazheng@jiazhengdeMacBook-Pro.local>
2019-05-19 19:56:14 +0800

    fix

--- src/main/java/club/own/site/controller/DataController.java
+++ src/main/java/club/own/site/controller/DataController.java
@@ -36,27 +36,25 @@ public class DataController extends BaseController{
         Util.sleep(1000);
         long[] ticks = processor.getSystemCpuLoadTicks();
         List<String> res = Lists.newArrayList();
-        for (int i = 0; i < ticks.length; i++) {
+        long user = ticks[CentralProcessor.TickType.USER.getIndex()] - prevTicks[CentralProcessor.TickType.USER.getIndex()];
+        long nice = ticks[CentralProcessor.TickType.NICE.getIndex()] - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
+        long sys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()] - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
+        long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()] - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
+        long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()] - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
+        long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()] - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
+        long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()] - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()] - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
+        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
+        CpuInfo cpuInfo = new CpuInfo();
 
-            long user = ticks[CentralProcessor.TickType.USER.getIndex()] - prevTicks[CentralProcessor.TickType.USER.getIndex()];
-            long nice = ticks[CentralProcessor.TickType.NICE.getIndex()] - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
-            long sys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()] - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
-            long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()] - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
-            long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()] - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
-            long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()] - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
-            long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()] - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
-            long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()] - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
-            long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
-            CpuInfo cpuInfo = new CpuInfo();
+        cpuInfo.setUser(NumberUtils.formatFloat(user  * 100D / totalCpu));
+        cpuInfo.setSys(NumberUtils.formatFloat(sys * 100D / totalCpu));
+        cpuInfo.setWait(NumberUtils.formatFloat(iowait * 100D / totalCpu));
+        cpuInfo.setIdle(NumberUtils.formatFloat(idle * 100D / totalCpu));
+        cpuInfo.setOther(NumberUtils.formatFloat((steal + irq + softirq + nice)  * 100D));
 
-            cpuInfo.setUser(NumberUtils.formatFloat(user  * 100D / totalCpu));
-            cpuInfo.setSys(NumberUtils.formatFloat(sys * 100D / totalCpu));
-            cpuInfo.setWait(NumberUtils.formatFloat(iowait * 100D / totalCpu));
-            cpuInfo.setIdle(NumberUtils.formatFloat(idle * 100D / totalCpu));
-            cpuInfo.setOther(NumberUtils.formatFloat((steal + irq + softirq + nice)  * 100D));
+        res.add(toEChartsPieData(cpuInfo));
 
-            res.add(toEChartsPieData(cpuInfo));
-        }
         return JSON.toJSONString(res);
     }
 
@@ -70,13 +68,11 @@ public class DataController extends BaseController{
         for (OSFileStore fs : fsArray) {
             long usable = fs.getUsableSpace();
             long total = fs.getTotalSpace();
-            if (fs.getType().toLowerCase().equals("ntfs") || fs.getType().toLowerCase().equals("apfs")) {
-                diskInfo.setTotal(NumberUtils.formatFloat(total / 1024d /1024d / 10d));
-                diskInfo.setFree(NumberUtils.formatFloat(usable / 1024d /1024d / 10d));
-                diskInfo.setUsed(NumberUtils.formatFloat((total - usable) / 1024d /1024d / 10d));
-                diskInfo.setUsePercent(NumberUtils.formatFloat((total - usable) * 100 / total));
-                break;
-            }
+            diskInfo.setTotal(NumberUtils.formatFloat(total / 1024d /1024d / 1024d / 10d));
+            diskInfo.setFree(NumberUtils.formatFloat(usable / 1024d /1024d / 1024d  / 10d));
+            diskInfo.setUsed(NumberUtils.formatFloat((total - usable) / 1024d /1024d / 1024d  / 10d));
+            diskInfo.setUsePercent(NumberUtils.formatFloat((total - usable) * 100 / total));
+            break;
         }
         return toEChartsBarData(diskInfo);
     }
